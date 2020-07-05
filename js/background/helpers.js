15ddb998bd521200075a0e00a5bfee74e2994cfc
achatainga <a.chataing.a@gmail.com>
2020-07-04 22:03:39 -0400

    New commit

--- js/background/helpers.js
+++ js/background/helpers.js
@@ -244,3 +244,70 @@ var isEmpty = ( obj ) => {
 	
 	return JSON.stringify( obj ) === JSON.stringify( {} );
 }
+
+var detectBrowser = ( type ) => {
+	var browser = {
+		"opera": ( () => {
+			return (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+		} ),
+		"firefox": ( () => {
+			return typeof InstallTrigger !== 'undefined';
+		} ),
+		"safari": ( () => {
+			return /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+		} ),
+		"ie": ( () => {
+			return /*@cc_on!@*/false || !!document.documentMode;
+		} ),
+		"edge": ( () => {
+			return !isIE && !!window.StyleMedia;
+		} ),
+		"chrome": ( () => {
+			return !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+		} ),
+		"edge_chromium": ( () => {
+			return sChrome && (navigator.userAgent.indexOf("Edg") != -1);
+		} ),
+		"blink": ( () => {
+			return (isChrome || isOpera) && !!window.CSS;
+		} )
+	}
+
+	return browser[ type ]();
+	// // Opera 8.0+
+	// var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+
+	// // Firefox 1.0+
+	// var isFirefox = typeof InstallTrigger !== 'undefined';
+
+	// // Safari 3.0+ "[object HTMLElementConstructor]" 
+	// var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+	// // Internet Explorer 6-11
+	// var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+	// // Edge 20+
+	// var isEdge = !isIE && !!window.StyleMedia;
+
+	// // Chrome 1 - 79
+	// var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+	// // Edge (based on chromium) detection
+	// var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+	// // Blink engine detection
+	// // var isBlink = (isChrome || isOpera) && !!window.CSS;
+
+
+
+	// var output = 'Detecting browsers by ducktyping:<hr>';
+	// output += 'isFirefox: ' + isFirefox + '<br>';
+	// output += 'isChrome: ' + isChrome + '<br>';
+	// output += 'isSafari: ' + isSafari + '<br>';
+	// output += 'isOpera: ' + isOpera + '<br>';
+	// output += 'isIE: ' + isIE + '<br>';
+	// output += 'isEdge: ' + isEdge + '<br>';
+	// output += 'isEdgeChromium: ' + isEdgeChromium + '<br>';
+	// output += 'isBlink: ' + isBlink + '<br>';
+	// document.body.innerHTML = output;
+}
