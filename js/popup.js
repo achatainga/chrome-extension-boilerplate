fcdca2185ca0ab5372d7c03a29d8bf77d856d1b1
achatainga <a.chataing.a@gmail.com>
2020-07-30 03:31:59 -0400

    New commit

--- js/popup.js
+++ js/popup.js
@@ -4,7 +4,8 @@ function initialize() {
     $( document ).ready( function() {
         chrome.tabs.query( { currentWindow: true, active: true }, function( tabs ) {
             var currentTab = tabs[ 0 ];
-            console.log( 'https://couponifier.com/ext_store.php?link=' + helpers.extractHostname( currentTab.url ) );
+            // var parsed = psl.parse('one.two.roothost.co.uk');
+            console.log( 'https://couponifier.com/ext_store.php?link=' + psl.parse( ( currentTab.url ) );
             $( '#couponifier_iframe' ).attr( 'src', 'https://couponifier.com/ext_store.php?link=' + helpers.extractHostname( currentTab.url ) );
             // chrome.runtime.sendMessage( { host: helpers.extractHostname( currentTab.url ), action: "get_data_from_api" }, async function( response ) {
                 
