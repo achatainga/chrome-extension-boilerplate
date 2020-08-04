d591a946bda17a2e45f45958ccc4efe111aa3da7
achatainga <a.chataing.a@gmail.com>
2020-08-03 20:09:53 -0400

    New commit

--- js/content.js
+++ js/content.js
@@ -37,8 +37,10 @@ async function get_deals() {
 	var url = window.location.href;
 	chrome.runtime.sendMessage( { url: url, action: "number_of_store_offers" }, async function( response ) {
 		if ( !nullOrundefined( response ) && !isEmpty( response ) ) {
-			chrome.runtime.sendMessage( { action: "update_icon", text: response.toString(), url: url }, async function( response ) {
-			} );
+			if ( response != -1 ) {
+				chrome.runtime.sendMessage( { action: "update_icon", text: response.toString(), url: url }, async function( response ) {
+				} );
+			}
 		}
 	} );
 }
