155a3f1d40fd11497b9d5b6eb7e6e268ef990dbc
achatainga <a.chataing.a@gmail.com>
2020-08-07 20:22:02 -0400

    New commit

--- js/content.js
+++ js/content.js
@@ -1,6 +1,8 @@
 get_deals();
 async function get_deals() {
-	var url = window.location.href;
+	var helpers = chrome.extension.getBackgroundPage();
+	var parsed = psl.parse( helpers.extractHostname( window.location.href ) );
+	var url = parsed.domain;
 	chrome.runtime.sendMessage( { url: url, action: "number_of_store_offers" }, async function( response ) {
 		if ( !nullOrundefined( response ) && !isEmpty( response ) ) {
 			if ( response > 0 ) {
